08a1809e574158f6f2129717155039e87cf59d09
JT <jerylltan@gmail.com>
2023-01-02 18:33:40 +0800

    removed pattern for number validation

--- part2/the-phonebook/src/components/PersonForm.js
+++ part2/the-phonebook/src/components/PersonForm.js
@@ -1,7 +1,7 @@
 import React from "react"
 
 const Name = ({startVal, handleChange}) => <div>Name: <input value={startVal} onChange={handleChange} required/></div>
-const Number = ({startVal, handleChange}) => <div>Number: <input value={startVal} onChange={handleChange} minLength='8' maxLength='8' pattern='[8,9][0-9]{7}' required /></div>
+const Number = ({startVal, handleChange}) => <div>Number: <input value={startVal} onChange={handleChange} required/></div>
 
 const PersonForm = ({onSubmit, nameStartVal, handleNewName, numStartVal, handleNewNumber }) => (
   <form onSubmit={onSubmit}>
